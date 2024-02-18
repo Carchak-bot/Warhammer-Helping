75fc53ca7c48646b266d5641ce8650731e6ca971
clem27370 <clem27370@gmail.com>
2024-02-18 20:24:25 +0100

    ajout connexion

---
+++ Controlleur/Ajax/control_connexion.php
@@ -0,0 +1,21 @@
+<?php 
+require "./../../Modele/modele.php";
+session_start();
+
+$user="";
+$MDP="";
+
+	if( isset($_POST['user']) && isset($_POST['MDP']) ){
+		$user= $_POST['user'];
+		$MDP= $_POST['MDP'];
+
+     	$request = 'SELECT count(*) FROM compte WHERE Nom_de_compte = "'.$user.'" and Mot_de_passe = "'.$MDP.'";'; 
+        $tabUser = selectDatabase($request);
+		if ($tabUser[0]["count(*)"] == '1'){
+            $_SESSION['user'] = $user;
+            exit();
+        } else {
+            echo "Nom d'utilisateur ou mot de passe incorrecte";
+            exit();
+        }
+    }
---
+++ assets/Js/commandes.js
@@ -0,0 +1,23 @@
+  //Fonction Ajax pour la connexion au site
+  $(document).ready(function() {
+    $("#conn").click(function() {
+
+    $.ajax({
+        url:  "./../Controlleur/Ajax/control_connexion.php",
+        type: "POST",
+        data: {
+            'user' : $("#user").val(),
+            'MDP' : $("#MDP").val()
+            },
+        success: function(data){
+            if(data == "Success"){
+            // Le membre est connecté.
+            window.location = "./../index.php";
+            }else{
+            // Le membre n'a pas été connecté. (data vaut ici "failed")
+            alert(data);
+            }
+        }
+    });
+    });
+});
