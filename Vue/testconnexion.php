7754e19a83efbe7c9868dc92292cf6d9f63f9ae3
clem27370 <clem27370@gmail.com>
2024-02-19 21:31:25 +0100

    Formulaire connexion fonctionnel en PHP

---
+++ Vue/testconnexion.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<meta charset="UTF-8" />
+
+<link rel="stylesheet" href="./../assets/css/styleconnexion.css" />
+
+<div id="container">
+    <form action="./../Controlleur/testconnexion.php" method="post">
+        <h1>Connexion</h1>
+ 
+        <label><b>Nom d'utilisateur</b></label>
+        <input type="text" placeholder="Entrer le nom d'utilisateur" name="pseudo" required />
+        <br />
+        <label><b>Mot de passe</b></label>
+        <input type="password" placeholder="Entrer le mot de passe" name="mdp" required />
+        <br />
+    <input type="submit" name="connexion" value="Connexion" />
+    <a href="Inscription.php">Pas de compte ? S'inscrire</a>
+
+    <?php
+        if(isset($_GET['erreur'])){
+        $err = $_GET['erreur'];
+        if($err==1)
+            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        }
+ ?>
+    </form>
+</div>
